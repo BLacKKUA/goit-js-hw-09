0a349a2d15b2ecf5e3945bf462670273de3dd642
Roman Valko <rvalko.dev@gmail.com>
2022-06-16 01:43:12 +0300

    third task done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  submit: document.querySelector('button[type="submit"]'),
+}
+
+refs.submit.addEventListener('click', startCreatePromise)
+
+function startCreatePromise(event) {
+  event.preventDefault();
+  let delayTotal = parseInt(refs.delay.value);
+  for (let i = 0; i < refs.amount.value; i++) {
+    createPromise(i, delayTotal)
+    delayTotal += parseInt(refs.step.value);
   }
+}
+
+
+function createPromise(position, delay) {
+const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+  promise.then(value => console.log(value)).catch(error => console.log(error))
+    }, delay)
+  })
 }
